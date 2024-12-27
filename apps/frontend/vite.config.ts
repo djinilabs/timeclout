e64eaba8e7d121d185b2a3d71e5dd741af2e3c1b
Pedro Teixeira <i@pgte.me>
2024-12-27 15:54:13 +0000

    FE proxy

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3333",
+        changeOrigin: true,
+      },
+    },
+  },
 });
