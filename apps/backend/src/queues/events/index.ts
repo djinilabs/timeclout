809c39bb5a41757e7d946c2ac14fcda88cbfa86d
Pedro Teixeira <i@pgte.me>
2025-01-10 10:22:30 +0000

    book time off wip

--- apps/backend/src/queues/events/index.ts
+++ apps/backend/src/queues/events/index.ts
@@ -3,10 +3,7 @@ import { handleQueueEvent } from "../handleQueueEvent";
 import { EventBusEvent } from "@/event-bus";
 import { handleCreateLeaveRequest } from "./createLeaveRequest";
 
-export const handler = async (
-  event: SQSEvent,
-  context: Context
-): Promise<SQSBatchResponse> => {
+export const handler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
   return handleQueueEvent<EventBusEvent>(
     event,
     async (payload: EventBusEvent) => {
@@ -14,6 +11,8 @@ export const handler = async (
         case "createLeaveRequest":
           await handleCreateLeaveRequest(payload);
           break;
+        default:
+          throw new Error(`Unknown event key: ${payload.key}`);
       }
     }
   );
