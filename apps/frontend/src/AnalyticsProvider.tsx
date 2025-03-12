bfe46350a1d133bd6f31f50e03556ee952b82ad3
Pedro Teixeira <i@pgte.me>
2025-03-12 11:11:20 +0000

    posthog

---
+++ apps/frontend/src/AnalyticsProvider.tsx
@@ -0,0 +1,18 @@
+import { FC, PropsWithChildren } from "react";
+import { PostHogProvider } from "posthog-js/react";
+
+const postHogOptions = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
+
+export const AnalyticsProvider: FC<PropsWithChildren> = ({ children }) => {
+  const key = process.env.VITE_PUBLIC_POSTHOG_KEY;
+  if (!key) {
+    return children;
+  }
+  return (
+    <PostHogProvider apiKey={key} options={postHogOptions}>
+      {children}
+    </PostHogProvider>
+  );
+};
--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import { FC, useMemo, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { Provider as UrqlProvider } from "urql";
 import { SessionProvider } from "next-auth/react";
 import { ErrorBoundary, init as initSentry, withProfiler } from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRoutes } from "./Routes";
 import { createClient } from "./graphql/graphql-client";
-import { BrowserRouter } from "react-router-dom";
 import { Suspense } from "./components/stateless/Suspense";
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
 import { dynamicActivate } from "./i18n";
 import { RequiresSession } from "./components/RequiresSession";
+import { AnalyticsProvider } from "./AnalyticsProvider";
 
 const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
 
@@ -41,21 +42,26 @@ const AppComponent: FC = () => {
         </div>
       )}
     >
-      <I18nProvider i18n={i18n}>
-        <BrowserRouter>
-          <QueryClientProvider client={queryClient}>
-            <SessionProvider refetchWhenOffline={false} basePath="/api/v1/auth">
-              <UrqlProvider value={client}>
-                <RequiresSession>
-                  <Suspense>
-                    <AppRoutes />
-                  </Suspense>
-                </RequiresSession>
-              </UrqlProvider>
-            </SessionProvider>
-          </QueryClientProvider>
-        </BrowserRouter>
-      </I18nProvider>
+      <AnalyticsProvider>
+        <I18nProvider i18n={i18n}>
+          <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+              <SessionProvider
+                refetchWhenOffline={false}
+                basePath="/api/v1/auth"
+              >
+                <UrqlProvider value={client}>
+                  <RequiresSession>
+                    <Suspense>
+                      <AppRoutes />
+                    </Suspense>
+                  </RequiresSession>
+                </UrqlProvider>
+              </SessionProvider>
+            </QueryClientProvider>
+          </BrowserRouter>
+        </I18nProvider>
+      </AnalyticsProvider>
     </ErrorBoundary>
   );
 };
