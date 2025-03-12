96c59802359b20b4c1de6c29ab967909816516da
Pedro Teixeira <i@pgte.me>
2025-03-12 11:50:34 +0000

    posthog

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -49,15 +49,9 @@ export default defineConfig({
   },
   define: {
     "process.env": {
-      VITE_PUBLIC_SENTRY_DSN: JSON.stringify(
-        process.env.VITE_PUBLIC_SENTRY_DSN
-      ),
-      VITE_PUBLIC_POSTHOG_KEY: JSON.stringify(
-        process.env.VITE_PUBLIC_POSTHOG_KEY
-      ),
-      VITE_PUBLIC_POSTHOG_HOST: JSON.stringify(
-        process.env.VITE_PUBLIC_POSTHOG_HOST
-      ),
+      VITE_PUBLIC_SENTRY_DSN: process.env.VITE_PUBLIC_SENTRY_DSN,
+      VITE_PUBLIC_POSTHOG_KEY: process.env.VITE_PUBLIC_POSTHOG_KEY,
+      VITE_PUBLIC_POSTHOG_HOST: process.env.VITE_PUBLIC_POSTHOG_HOST,
     },
     global: "globalThis",
   },
