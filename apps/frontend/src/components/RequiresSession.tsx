cc9357ee156a46857e3abd2b5f1dd7677a78fefb
Pedro Teixeira <i@pgte.me>
2025-05-23 15:49:57 +0100

    login no redirect by default

--- apps/frontend/src/components/RequiresSession.tsx
+++ apps/frontend/src/components/RequiresSession.tsx
@@ -1,9 +1,13 @@
 import { type FC, type PropsWithChildren } from "react";
 import { useSession } from "next-auth/react";
 import Login from "./Login";
+import { Loading } from "./particles/Loading";
 
 export const RequiresSession: FC<PropsWithChildren> = ({ children }) => {
   const { status } = useSession({ required: false });
+  if (status === "loading") {
+    return <Loading />;
+  }
   if (status === "unauthenticated") {
     return <Login />;
   }
