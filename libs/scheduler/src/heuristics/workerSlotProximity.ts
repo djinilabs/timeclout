d209852e4fd9d825c667c03a555021c17d0cccd5
Pedro Teixeira <i@pgte.me>
2025-05-31 16:55:14 +0100

    heiristics slight refactor 1

--- libs/scheduler/src/heuristics/workerSlotProximity.ts
+++ libs/scheduler/src/heuristics/workerSlotProximity.ts
@@ -1,45 +1,52 @@
-import { ShiftScheduleHeuristic, SlotWorker } from "../types";
+import { ShiftScheduleHeuristic, SlotWorker, ShiftSchedule } from "../types";
 import { countTotalUniqueWorkers } from "../utils/countTotalUniqueWorkers";
 import { stdDev } from "../utils/standardDeviation";
 
+export const calculateExpectedWorkerSlotProximity = (
+  schedule: ShiftSchedule
+) => {
+  const totalShiftCount = schedule.shifts.length;
+  const totalWorkerCount = countTotalUniqueWorkers(schedule);
+  return totalShiftCount / totalWorkerCount;
+};
+
+export const calculateWorkerSlotProximityDeviation = (
+  schedule: ShiftSchedule
+) => {
+  const expectedShiftPeriodPerWorker =
+    calculateExpectedWorkerSlotProximity(schedule);
+
+  const workerShifts: Map<SlotWorker, { shiftIndex: number }[]> = new Map();
+
+  // Group shifts by worker and sort by start time
+  schedule.shifts.forEach((shift, shiftIndex) => {
+    const shifts = workerShifts.get(shift.assigned) ?? [];
+    shifts.push({
+      shiftIndex,
+    });
+    workerShifts.set(shift.assigned, shifts);
+  });
+
+  // Sort shifts for each worker by start time
+  for (const shifts of workerShifts.values()) {
+    shifts.sort((a, b) => a.shiftIndex - b.shiftIndex);
+  }
+
+  const proximities: number[] = [];
+
+  // Calculate proximities between consecutive shifts for each worker
+  for (const shifts of workerShifts.values()) {
+    for (let i = 1; i < shifts.length; i++) {
+      const proximityInShiftIndex =
+        shifts[i].shiftIndex - shifts[i - 1].shiftIndex;
+      proximities.push(proximityInShiftIndex / expectedShiftPeriodPerWorker);
+    }
+  }
+
+  return stdDev(1, proximities);
+};
+
 export const workerSlotProximityHeuristic: ShiftScheduleHeuristic = {
   name: "Worker Slot Proximity",
-  eval: (schedule) => {
-    // Here we want to calculate the standard deviation of the proximity between each consecutive shift for each worker
-    // The proximity is the difference in minutes between the start of the current shift and the end of the previous shift
-    // We want to minimize this standard deviation
-    // First, we need to calculate the expected proximity (also in minutes) between each consecutive shift for each worker
-    const totalShiftCount = schedule.shifts.length;
-    const totalWorkerCount = countTotalUniqueWorkers(schedule);
-    const expectedShiftPeriodPerWorker = totalShiftCount / totalWorkerCount;
-
-    const workerShifts: Map<SlotWorker, { shiftIndex: number }[]> = new Map();
-
-    // Group shifts by worker and sort by start time
-    schedule.shifts.forEach((shift, shiftIndex) => {
-      const shifts = workerShifts.get(shift.assigned) ?? [];
-      shifts.push({
-        shiftIndex,
-      });
-      workerShifts.set(shift.assigned, shifts);
-    });
-
-    // Sort shifts for each worker by start time
-    for (const shifts of workerShifts.values()) {
-      shifts.sort((a, b) => a.shiftIndex - b.shiftIndex);
-    }
-
-    const proximities: number[] = [];
-
-    // Calculate proximities between consecutive shifts for each worker
-    for (const shifts of workerShifts.values()) {
-      for (let i = 1; i < shifts.length; i++) {
-        const proximityInShiftIndex =
-          shifts[i].shiftIndex - shifts[i - 1].shiftIndex;
-        proximities.push(proximityInShiftIndex / expectedShiftPeriodPerWorker);
-      }
-    }
-
-    return stdDev(1, proximities);
-  },
+  eval: (schedule) => calculateWorkerSlotProximityDeviation(schedule),
 };
