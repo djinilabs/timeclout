c56356e75be7c4a3976dd084ecb6ad298818bc56
Pedro Teixeira <i@pgte.me>
2025-06-11 09:20:02 +0100

    check latest app version wip 1

---
+++ libs/graphql/src/schema/version/resolvers/Query/latestAppVersion.ts
@@ -0,0 +1,8 @@
+import type { QueryResolvers } from "./../../../../types.generated";
+
+export const latestAppVersion: NonNullable<
+  QueryResolvers["latestAppVersion"]
+> = async () => {
+  const { version } = await import("../../../../../../../package.json");
+  return version;
+};
