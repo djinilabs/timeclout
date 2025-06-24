c56167501eb925f654318d95ffdd1fc3d8431d47
Pedro Teixeira <i@pgte.me>
2025-06-24 10:29:16 +0100

    login button styles fix

--- apps/frontend/src/components/atoms/Login.tsx
+++ apps/frontend/src/components/atoms/Login.tsx
@@ -34,7 +34,7 @@ const Login: FC = () => {
             <div>
               <Button
                 onClick={handleSignIn}
-                className="w-full justify-center"
+                className="w-full justify-center relative inline-flex items-center rounded-md bg-teal-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-teal-500 hover:scale-110 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-teal-600 whitespace-nowrap disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-teal-600 disabled:hover:scale-100 transition duration-300"
                 aria-label="Sign in to your account"
               >
                 <Trans>Sign in</Trans>
