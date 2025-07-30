41c112887410f6c224044401442f9e5a1c0f1885
Pedro Teixeira <i@pgte.me>
2025-07-30 10:11:53 +0100

    fix

--- libs/scheduler-worker/src/schedulerWorker.ts
+++ libs/scheduler-worker/src/schedulerWorker.ts
@@ -1,10 +1,16 @@
 // scheduler worker
 
+import { i18n } from "@/locales";
 import { Scheduler } from "@/scheduler";
 
 let scheduler: Scheduler | undefined;
 
 console.log("SchedulerWorker: starting");
+
+i18n.loadAndActivate({
+  locale: "en",
+  messages: {},
+});
 
 self.onmessage = (event) => {
   console.log("SchedulerWorker: onmessage", event.data);
