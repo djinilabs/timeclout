309b3d9b489c1d0d48933e16edf9cc6268c43804
Pedro Teixeira <i@pgte.me>
2025-10-27 17:40:57 +0000

    certificates and other stuffs

--- apps/backend/src/plugins/custom-domain/index.js
+++ apps/backend/src/plugins/custom-domain/index.js
@@ -2,7 +2,7 @@ module.exports = {
   deploy: {
     start: async ({ cloudformation, stage }) => {
       const customDomain = process.env.TT3_CUSTOM_DOMAIN;
-      const CertificateArn = process.env.TT3_CERTIFICATE_ARN;
+      const CertificateArn = process.env.AWS_CERTIFICATE_ARN;
       const HostedZoneId = process.env.TT3_ZONE_ID;
       console.log(`Custom domain: stage = ${stage}, custom domain = ${customDomain}`);
       if (customDomain) {
