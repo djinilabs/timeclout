0aa5f6247171505468e81249d2c3c5f606ae0f78
Pedro Teixeira <i@pgte.me>
2025-11-12 14:29:30 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import path from "path";
 
 import { lingui } from "@lingui/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vitest/config";
 import graphqlLoader from "vite-plugin-graphql-loader";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
